de912e6996313a1b57edd5cf304281fe131c0d94
duidae <duidae@gmail.com>
2019-05-20 14:36:39 +0800

    Resolve warnings

--- src/components/ControlPanel/Header.tsx
+++ src/components/ControlPanel/Header.tsx
@@ -5,9 +5,9 @@ export class Header extends React.Component {
     public render() {
         return (
             <div className="Header">
-                <img src="urlogo.png"/>
-                <a href="https://www.facebook.com/urbanrenewaleye" target="_blank">
-                    <img src="fblogo.png"/>
+                <img src="urlogo.png" alt="都市更新天眼通"/>
+                <a href="https://www.facebook.com/urbanrenewaleye" target="_blank" rel="noopener noreferrer">
+                    <img src="fblogo.png" alt="都市更新天眼通粉專"/>
                 </a>
             </div>
         );
