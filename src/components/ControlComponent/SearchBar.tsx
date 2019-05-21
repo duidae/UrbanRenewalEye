a4a208055f515b97dce903b14591e7f8b518554b
duidae <duidae@gmail.com>
2019-05-21 10:11:01 +0800

    Rewrite search bar with functional component

--- src/components/ControlComponent/SearchBar.tsx
+++ src/components/ControlComponent/SearchBar.tsx
@@ -1,44 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "components/ControlComponent/SearchBar.scss";
 
 declare const google: any;
 
-export class SearchBar extends React.Component {
-    private searchBox: any;
-
-    private onPlaceChange = () => {
-        const places = this.searchBox.getPlaces();
-        if (places.length === 0) {
-            return;
-        }
-
-        // For each place, get the icon, name and location.
-        let bounds = new google.maps.LatLngBounds();
-        places.forEach((place: any) => {
-            if (!place.geometry) {
-                console.log("Returned place contains no geometry");
+export const SearchBar: React.FC = () => {
+    useEffect(() => {
+        const searchBox = new google.maps.places.SearchBox(document.getElementById("searchbox"));
+        searchBox.addListener('places_changed', () => {
+            const places = searchBox.getPlaces();
+            if (places.length === 0) {
                 return;
             }
-            if (place.geometry.viewport) {
-                bounds.union(place.geometry.viewport);
-            } else {
-                bounds.extend(place.geometry.location);
-            }
+
+            // For each place, get the icon, name and location.
+            let bounds = new google.maps.LatLngBounds();
+            places.forEach((place: any) => {
+                if (!place.geometry) {
+                    console.log("Returned place contains no geometry");
+                    return;
+                }
+                if (place.geometry.viewport) {
+                    bounds.union(place.geometry.viewport);
+                } else {
+                    bounds.extend(place.geometry.location);
+                }
+            });
+            //this.map.fitBounds(bounds);
         });
-        //this.map.fitBounds(bounds);
-    };
+    });
 
-    componentDidMount() {
-        this.searchBox = new google.maps.places.SearchBox(document.getElementById("searchbox"));
-        this.searchBox.addListener('places_changed', this.onPlaceChange);
-    }
-
-    public render() {
-        return (
-            <div className="SearchBar">
-                <input id="searchbox" type="text" placeholder="看看我家在哪?"/>
-                <button type="button"><img src="mylocation_grey.png" alt="my location"/></button>
-            </div>
-        );
-    }
+    return (
+        <div className="SearchBar">
+            <input id="searchbox" type="text" placeholder="看看我家在哪?"/>
+            <button type="button"><img src="mylocation_grey.png" alt="my location"/></button>
+        </div>
+    );
 }
